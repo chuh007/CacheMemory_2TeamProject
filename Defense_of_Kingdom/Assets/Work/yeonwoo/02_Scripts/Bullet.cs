eef859b4038baa051939d1161663ce82b7209a59
ByulDaram <0981syw@naver.com>
2025-07-08 19:48:59 +0900

    HeroFire(Temporary)

---
+++ Defense_of_Kingdom/Assets/Work/yeonwoo/02_Scripts/Bullet.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+   [SerializeField] private float _speed = 10f;
+
+    
+    private void Update() => transform.position += Vector3.right * (_speed * Time.deltaTime);
+    
+}
